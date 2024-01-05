5623366decf1bc967c5f723ff3f42dd8c7711c0e
el634dev <84924260+el634dev@users.noreply.github.com>
2024-01-05 16:31:16 -0500

    updated app.py

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 # Import necessary libraries
 from flask import Flask, render_template, url_for, redirect
 from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import InputRequired, Email, Length
+from models import LoginForm, User, RegisterForm, db
+from flask_sqlalchemy import SQLAlchemy
+from passlib.hash import pbkdf2_sha256
+from flask_login import login_required, login_user
+from models import login_man
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # Run source/venv/bin/activate
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Thisissecret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
 Bootstrap(app)
 
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
-    remember_session = BooleanField('Remember session')
-
-class RegisterForm(FlaskForm):
-    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
+db.init_app(app)
+login_man.init_app(app)
 
 # ------------------
 # Route to home page
@@ -26,31 +26,56 @@ def home_page():
     return render_template('index.html')
 
 # -------------------
+# Route for signup
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+    
+    if form.validate_on_submit():
+        # hashed_password = generate_password_hash(form.password.data, method='sha256')
+        new_user = User(username=form.username.data, password=form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+        return '<h1>New user has been created!</h1>'
+        # return '<h1>Welcome ' + form.username.data + '</h1>
+    return render_template('signup.html', form=form)
+
+# -------------------
+# Route for FAQ page
+@app.route('/faq')
+def faq():
+    return render_template('faq.html')
+
+# ------------------
+# Route for Features page
+@app.route('/features')
+def features():
+    return render_template('features.html')
+
+# -------------------
+# Route for dashboard
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
+
+# -------------------
 # Route for login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     
     if form.validate_on_submit():
-        return '<h1> Welcome ' + form.username.data + '</h1>'
-        return render_template('dashboard.html')
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if user.password == form.password.data:
+                login_user(user, remember=form.remember_session.data)
+                return redirect(url_for('dashboard'))
+        return '<h1>Invalid username or password</h1>'
+        # return '<h1> Welcome ' + form.username.data + '</h1>'
     return render_template('login.html', form=form)
-
-# -------------------
-# Route for signup
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    form = RegisterForm()
-
-    return render_template('signup.html', form=form)
-
-# -------------------
-# Route for dashboard
-@app.route('/dashboard')
-def dashboard():
-    return render_template('dashboard.html')
 
 # ---------------------------
 if __name__ == "__main__":
-    config()
     app.run(debug=True)
